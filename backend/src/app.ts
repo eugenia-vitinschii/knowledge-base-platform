683d727f165fd06b03ab9e9b99168f5003778e6d
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-23 17:34:21 +0200

    feat(user.routes): create user route and import inside app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
+import userRoutes from "./routes/user.routes"
+
 export const app = express()
 
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', userRoutes)
 
 app.get('/', (_req, res) => {
    res.json({ status: 'ok' })
---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,10 @@
+//user routes
+
+import { Router } from "express";
+import { userController } from "../controllers/user.controller";
+
+const router = Router()
+
+router.post('/', userController.create)
+
+export default router
