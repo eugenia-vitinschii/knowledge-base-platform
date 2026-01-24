cc18b194311eb927f6232beec2d01e0395689328
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-24 16:14:23 +0200

    feat(database): connect MongoDB  to the app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
+import 'dotenv/config'
 import { app } from './app'
+import { connectDB } from './config/database'
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
-   console.log(`Backends running on http://localhost:${PORT}`)
-})
+const start = async () => {
+   await connectDB()
+
+   app.listen(PORT, () => {
+      console.log(`Backends running on http://localhost:${PORT}`)
+   })
+}
+start()
