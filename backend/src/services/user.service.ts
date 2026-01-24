9e44a8d5316cdf927d8bb67b105f9ef8a345ebfa
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-24 18:15:15 +0200

    feat(user.service): hash password create user

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,14 +1,37 @@
 //user service
 
-import { User } from "../models/user.model";
+import bcrypt from 'bcrypt'
+import { UserModel } from "../models/user.model";
 import { CreateUserDto } from "../dtos/create-user.dto";
 
+const SALT_ROUNDS = 10
+
 class UserService {
-   async create(dto: CreateUserDto): Promise<User> {
-      throw new Error("Not implemented")
+   async create(dto: CreateUserDto) {
+      const existingUser = await UserModel.findOne({ email: dto.email })
+
+      if (existingUser) {
+         throw new Error("User with this email already exists!")
+      }
+
+      const hashedPassword = await bcrypt.hash(dto.password, SALT_ROUNDS)
+
+      const user = await UserModel.create({
+         name: dto.name,
+         email: dto.email,
+         password: hashedPassword,
+         role: dto.role
+      })
+
+      return {
+         id: user._id,
+         name: user.name,
+         email: user.email,
+         role: user.role,
+      }
    }
 
-   async findById(id: string): Promise<User | null> {
+   async findById(id: string) {
       throw new Error("Not implemented")
    }
 }
