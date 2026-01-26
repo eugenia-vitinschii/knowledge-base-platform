93249720c906e3abe32db3c9d620522456d7aa79
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-26 18:10:45 +0200

    feat(auth.routes): add POST /login route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import cors from 'cors'
 
 import userRoutes from "./routes/user.routes"
-
+import authRoutes from "./routes/auth.routes"
 export const app = express()
 
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 app.get('/', (_req, res) => {
    res.json({ status: 'ok' })
---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+//auth routes
+
+import { Router } from "express";
+import { authController } from "../controllers/auth.controller";
+
+const router = Router()
+
+router.post('/login', authController.login)
+
+export default router
