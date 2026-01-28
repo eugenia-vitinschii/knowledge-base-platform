2d88bed37f81116c6574fc1fdf97d3399de53528
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-28 17:09:31 +0200

    feat(article.enums): create enums for: difficulty, type, status, category

---
+++ backend/src/common/enums/article.enums.ts
@@ -0,0 +1,31 @@
+//article enums
+
+export enum ArticleDifficulty {
+   BEGINNER = 'beginner',
+   INTERMEDIATE = 'intermediate',
+   ADVANCED = 'advanced'
+}
+
+export enum ArcticleType {
+   ARTICLE = 'article',
+   DOCUMENTATION = 'documentation',
+   GUIDE = 'guide',
+   REFERENCE = 'reference',
+}
+
+export enum ArticleStatus {
+   DRAFT = 'draft',
+   REVIEW = 'review',
+   PUBLISHED = 'published',
+}
+
+export enum ArticleCategory {
+   HTML_CSS = 'html-css',
+   JAVASCRIPT = 'javascript',
+   TYPESCRIPT = 'typescript',
+   VUE = 'vue',
+   PINIA = 'pinia',
+   ZOD = 'zod',
+   FORMS_VALIDATIONS = 'forms-validation',
+   BACKEND_BASIS = 'backend-basis',
+}
