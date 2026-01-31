f6279591a260bbee888f2505cf8e59d057d26845
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-31 15:30:28 +0200

    feat(article.routes): add protected rputes with role-based middleware

--- backend/src/routes/article.routes.ts
+++ backend/src/routes/article.routes.ts
@@ -3,10 +3,16 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { articleController } from "../controllers/article.controller";
+import { requiredRole } from "../middleware/role.middleware";
+import { Role } from "../common/enums/role.enum";
 
 const router = Router()
 
-router.post('/', authMiddleware, articleController.create)
+router.post('/', authMiddleware, requiredRole(Role.ADMIN, Role.EDITOR), articleController.create)
+router.patch('/:id', authMiddleware, requiredRole(Role.ADMIN, Role.EDITOR), articleController.updateContent)
+router.patch('/:id/status', authMiddleware, requiredRole(Role.ADMIN), articleController.updateStatus)
+router.delete('/:id', authMiddleware, requiredRole(Role.ADMIN), articleController.updateStatus)
+
 router.get('/', articleController.getPublished)
 router.get('/:slug', articleController.getBySlug)
 
