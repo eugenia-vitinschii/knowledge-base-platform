c3f8ae33e83162cec3532cfff5c34b77a99a3c19
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-31 15:27:54 +0200

    feat(article.service): extend CRUD logic with update contnet, status, find by id and delete

--- backend/src/services/article.service.ts
+++ backend/src/services/article.service.ts
@@ -2,10 +2,14 @@
 
 import { ArticleStatus } from "../common/enums/article.enums";
 import { CreateArticleDto } from "../dtos/create-article.dto";
+import { UpdateArticleDto } from "../dtos/update-article.dto";
 import { ArticleModel } from "../models/article.model";
+import { Role } from '../common/enums/role.enum'
 import { Types } from "mongoose";
 
+
 class ArticleService {
+   /* CREATE */
    async create(dto: CreateArticleDto, authorId: string) {
       const slug = this.generateSlug(dto.title)
 
@@ -17,7 +21,7 @@ class ArticleService {
       })
       return article
    }
-
+   /* FIND */
    async findAllPublished() {
       return ArticleModel.find({ status: ArticleStatus.PUBLISHED })
    }
@@ -25,8 +29,45 @@ class ArticleService {
       return ArticleModel.findOne({ slug, status: ArticleStatus.PUBLISHED })
    }
 
+   async findById(id: string) {
+      return await ArticleModel.findById(id)
+   }
+   /* UPDATE*/
+   async updateContent(articleId: string, userId: string, dto: UpdateArticleDto, role: Role) {
+      const article = await ArticleModel.findById(articleId)
+
+      if (!article) throw new Error('Article nor exist!')
+
+      this.checkArticleOwnership(article, userId, role)
+
+      if (dto.title && dto.title !== article.title) {
+         article.slug = this.generateSlug(dto.title)
+      }
+
+      Object.assign(article, dto)
+
+      return article.save()
+   }
+
+   async updateStatus(articleId: string, status: ArticleStatus) {
+      return ArticleModel.findByIdAndUpdate(articleId, { status }, { new: true })
+   }
+   /* DELETE*/
+   async delete(id: string) {
+      return await ArticleModel.findByIdAndDelete(id)
+   }
+   /* generate SLUG */
    private generateSlug(title: string): string {
       return title.toLocaleLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '')
+   }
+   /* check article ownership */
+   private checkArticleOwnership(article: any, userId: string, role: Role) {
+      const isAuthor = article.author.toString() === userId
+
+      if (isAuthor) return
+      if (role === Role.ADMIN || role === Role.EDITOR) return
+
+      throw new Error("Forbidden")
    }
 }
 
