47604161b1d0598f17a584421edac7e847f7171c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-31 15:29:07 +0200

    feat(articlle.controller): add HTTP handlers for update, status change and delete

--- backend/src/controllers/article.controller.ts
+++ backend/src/controllers/article.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 import { articleService } from "../services/article.service"
 
 class ArticleController {
+   /*CREATE*/
    create = async (req: Request, res: Response) => {
       const user = (req as any).user
 
@@ -11,7 +12,7 @@ class ArticleController {
 
       res.status(201).json(article)
    }
-
+   /* GET */
    getPublished = async (_req: Request, res: Response) => {
       const articles = await articleService.findAllPublished()
       res.json(articles)
@@ -25,6 +26,45 @@ class ArticleController {
       }
       const article = await articleService.findBySlug(slug)
       res.json(article)
+   }
+   /* UPDATE */
+   updateContent = async (req: Request, res: Response) => {
+      const user = (req as any).user
+      const { id } = req.params
+
+      if (Array.isArray(id)) {
+         return res.status(400).json({ message: 'Invalid article id' })
+      }
+
+      const article = await articleService.updateContent(
+         id,
+         user.id,
+         req.body,
+         user.role
+      )
+
+      res.json(article)
+   }
+
+   updateStatus = async (req: Request, res: Response) => {
+      const { id } = req.params
+      const { status } = req.body
+      if (Array.isArray(id)) {
+         return res.status(400).json({ message: 'Invalid article id' })
+      }
+      const article = await articleService.updateStatus(id, status)
+      res.json(article)
+   }
+   /* DELETE */
+   delete = async (req: Request, res: Response) => {
+      const { id } = req.params
+
+      if (Array.isArray(id)) {
+         return res.status(400).json({ message: 'Invalid article id' })
+      }
+      await articleService.delete(id)
+
+      res.status(204).send()
    }
 }
 
