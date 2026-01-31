2b80ca9045c599854302fb0d5780673226267501
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-01-31 15:32:43 +0200

    fix(auth.middleware, j t): fix JWT pauload typing and request user assignment

--- backend/src/common/utils/jwt.ts
+++ backend/src/common/utils/jwt.ts
@@ -1,13 +1,18 @@
 //jwt
 
 import jwt from "jsonwebtoken"
+import { Role } from "../enums/role.enum"
 
 const JWT_SECRET = process.env.JWT_SECRET!
+export interface JwtUserPayload {
+   id: string
+   role: Role
+}
 
 export const signToken = (payload: object) => {
    return jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' })
 }
 
-export const verifyToken = (token: string) => {
-   return jwt.verify(token, JWT_SECRET)
+export const verifyToken = (token: string): JwtUserPayload => {
+   return jwt.verify(token, JWT_SECRET) as JwtUserPayload
 }
--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -6,14 +6,14 @@ import { verifyToken } from "../common/utils/jwt";
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
    const authHeader = req.headers.authorization
 
-   if (!authHeader || !authHeader.startsWith('Bearer')) {
+   if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(401).json({ message: "No Token!" })
    }
-   const token = authHeader.split('')[1]
+   const token = authHeader.split(' ')[1]
 
    try {
       const payload = verifyToken(token)
-      req.user
+      req.user = payload
       next()
    } catch {
       return res.status(401).json({ messsge: "invalid token!" })
