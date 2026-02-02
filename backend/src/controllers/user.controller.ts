a01eaa234de5b1de9558fddf1336cae5c43b64c8
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-02 17:45:53 +0200

    fix(controller): forward errors to error middleware via next()

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,15 +1,15 @@
 //user controller
 
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { userService } from "../services/user.service";
 
 class UserController {
-   create = async (req: Request, res: Response) => {
+   create = async (req: Request, res: Response, next: NextFunction) => {
       try {
          const user = await userService.create(req.body)
          res.status(201).json(user)
       } catch (error) {
-         res.status(500).json({ message: "Internal server error" })
+         next(error)
       }
    }
 }
