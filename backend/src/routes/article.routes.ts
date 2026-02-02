2fe441baea77a3bad042b2fb2fc46eeccf539d43
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-02 16:14:22 +0200

    feat(article.routes): implement CreateArticleDto, UpdateArticleDto using validateResource helper

--- backend/src/routes/article.routes.ts
+++ backend/src/routes/article.routes.ts
@@ -5,13 +5,42 @@ import { authMiddleware } from "../middleware/auth.middleware";
 import { articleController } from "../controllers/article.controller";
 import { requiredRole } from "../middleware/role.middleware";
 import { Role } from "../common/enums/role.enum";
+import { CreateArticleDto } from "../dtos/create-article.dto";
+import { validateResource } from "../middleware/validateResource";
+import { UpdateArticleDto } from "../dtos/update-article.dto";
 
 const router = Router()
 
-router.post('/', authMiddleware, requiredRole(Role.ADMIN, Role.EDITOR), articleController.create)
-router.patch('/:id', authMiddleware, requiredRole(Role.ADMIN, Role.EDITOR), articleController.updateContent)
-router.patch('/:id/status', authMiddleware, requiredRole(Role.ADMIN), articleController.updateStatus)
-router.delete('/:id', authMiddleware, requiredRole(Role.ADMIN), articleController.delete)
+router.post(
+   '/',
+   authMiddleware,
+   requiredRole(Role.ADMIN, Role.EDITOR),
+   validateResource(CreateArticleDto),
+   articleController.create
+)
+
+router.patch(
+   '/:id',
+   authMiddleware,
+   requiredRole(Role.ADMIN, Role.EDITOR),
+   validateResource(UpdateArticleDto),
+   articleController.updateContent
+)
+
+router.patch(
+   '/:id/status',
+   authMiddleware,
+   requiredRole(Role.ADMIN),
+   validateResource(UpdateArticleDto),
+   articleController.updateStatus
+)
+
+router.delete(
+   '/:id',
+   authMiddleware,
+   requiredRole(Role.ADMIN),
+   articleController.delete
+)
 
 router.get('/', articleController.getPublished)
 router.get('/:slug', articleController.getBySlug)
