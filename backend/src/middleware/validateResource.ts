52dc9ea2a9a0387ac5bd85081956e67028dc9e01
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-02 15:07:30 +0200

    feat(validation): add express middleware for zod based request validation

---
+++ backend/src/middleware/validateResource.ts
@@ -0,0 +1,17 @@
+//validate resource
+
+import { ZodObject } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+export const validateResource = (schema: ZodObject<any>) => (req: Request, res: Response, next: NextFunction) => {
+   const result = schema.safeParse(req.body);
+
+   if (!result.success) {
+      return res.status(400).json({
+         message: 'Validation error',
+         errors: result.error.flatten().fieldErrors,
+      });
+   }
+   req.body = result.data;
+   next();
+}
