bb146e3f63c3418fc7a13730518e9876daa69024
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-03 18:38:36 +0200

    feat(login.service): handle login error with UnauthorizedError

--- backend/src/services/login.service.ts
+++ backend/src/services/login.service.ts
@@ -3,17 +3,18 @@
 import bcrypt from 'bcrypt'
 import { UserModel } from "../models/user.model"
 import { signToken } from "../common/utils/jwt"
+import { UnauthorizedError } from '../common/errors'
 
 class AuthService {
    async login(email: string, password: string) {
       const user = await UserModel.findOne({ email })
       if (!user) {
-         throw new Error("Invalid credentials")
+         throw new UnauthorizedError("Invalid credentials")
       }
 
       const isMatch = await bcrypt.compare(password, user.password)
       if (!isMatch) {
-         throw new Error("Invalid credentials")
+         throw new UnauthorizedError("Invalid credentials")
       }
 
       const token = signToken({
