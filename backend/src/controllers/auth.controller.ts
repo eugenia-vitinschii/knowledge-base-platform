5d0eb5e7d3cec4ae23a1044bd660be9fb86d643f
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-03 18:40:05 +0200

    feat(auth.controller): forward errors using next()

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,17 @@
 //auth controller 
 
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { authService } from "../services/login.service";
 
 class AuthController {
-   login = async (req: Request, res: Response) => {
+   login = async (req: Request, res: Response, next: NextFunction) => {
       try {
          const { email, password } = req.body
 
          const data = await authService.login(email, password)
          res.json(data)
       } catch (error) {
-         res.status(401).json({ message: "Invalid credentials" })
+         next(error)
       }
    }
 }
