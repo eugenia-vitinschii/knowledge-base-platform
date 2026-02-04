b117bb9a17ae1819f0ca9f926d96fce4de24296c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-04 17:40:35 +0200

    feat(App): add router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
-   <h2 class="heading">Lorem, ipsum dolor.</h2>
-   <h1 class="subheading">Lorem ipsum dolor sit amet consectetur..</h1>
-   <h3 class="body-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti beatae animi odio est
-      reiciendis tenetur perspiciatis iure vitae maiores exercitationem.</h3>
-   <p class="body-text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aut, molestiae labore id omnis mollitia adipisci,
-      animi repellendus sunt molestias sit voluptates blanditiis tenetur tempora illo ipsa, natus aliquid suscipit?
-   </p>
-
+   <router-view />
 </template>
+
+<script setup lang="ts"></script>
