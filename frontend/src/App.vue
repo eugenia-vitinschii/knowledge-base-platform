6016327b57981cacb2a92bb86772f99b30f736b1
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-05 19:45:28 +0200

    feat(ThemeToggle): create theme toggle component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-   <router-view />
+   <div class="wrapper">
+      <div class="header">header</div>
+      <div class="content">
+         <router-view />
+      </div>
+      <div class="footer">
+         <theme-toggle />
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione porro ab error repellendus, libero
+            perspiciatis labore velit aliquid accusantium, aperiam quae? Molestias, culpa animi alias sunt eius eum cum.
+            Asperiores corrupti illo, accusantium nulla impedit, repellendus dolore nam temporibus quidem blanditiis in
+            fuga!</p>
+      </div>
+   </div>
+
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ThemeToggle from './components/ui/ThemeToggle.vue';
+
+</script>
---
+++ frontend/src/components/ui/ThemeToggle.vue
@@ -0,0 +1,9 @@
+<template>
+   <button @click="theme.toogleTheme">{{ theme.theme }}</button>
+</template>
+
+<script setup lang="ts">
+import { useThemeStore } from '@/stores/theme/theme.store';
+
+const theme = useThemeStore()
+</script>
