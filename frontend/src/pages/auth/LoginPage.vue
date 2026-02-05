dfc712606ae441531106f824fda8857a31016e5a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-05 15:45:45 +0200

    feat(LoginPage): create basic form and implement auth.store logic

--- frontend/src/pages/auth/LoginPage.vue
+++ frontend/src/pages/auth/LoginPage.vue
@@ -1,7 +1,33 @@
 <template>
-   <p>login</p>
+   <div class="auth">
+      <div class="container">
+         <div class="auth__wrapper">
+            <div class="auth__container">
+               <form class="auth__form" @submit.prevent="auth.login({ email, password })">
+                  <input type="text" placeholder="email" v-model="email">
+                  <br>
+                  <input type="password" placeholder="password" v-model="password">
+                  <br>
+                  <button @click="auth.login({ email, password })">
+                     login
+                  </button>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+
 </template>
 
 <script setup lang="ts">
+//vue
+import { ref } from 'vue';
+//pinia
+import { useAuthStore } from '@/stores/auth/auth.store';
 
+//login
+const auth = useAuthStore()
+
+const email = ref('email');
+const password = ref('password')
 </script>
