d6581e421788519e042b7f4e49b96604f5f1d8da
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-06 17:46:33 +0200

    feat(LoginPage): implement UiInput component inside of login form

--- frontend/src/pages/auth/LoginPage.vue
+++ frontend/src/pages/auth/LoginPage.vue
@@ -3,12 +3,10 @@
       <div class="container">
          <div class="auth__wrapper">
             <div class="auth__container">
-               <form class="auth__form" @submit.prevent="auth.login({ email, password })">
-                  <input type="text" placeholder="email" v-model="email">
-                  <br>
-                  <input type="password" placeholder="password" v-model="password">
-                  <br>
-                  <button @click="auth.login({ email, password })">
+               <form class="auth__form" @submit.prevent="submit">
+                  <ui-input v-model="email" label="Email" type="email" />
+                  <ui-input v-model="password" label="Password" type="password" />
+                  <button type="submit">
                      login
                   </button>
                </form>
@@ -20,14 +18,21 @@
 </template>
 
 <script setup lang="ts">
-//vue
+/*COMPONENTS */
+import UiInput from '@/components/ui/UiInput.vue';
+
+/* VUE */
 import { ref } from 'vue';
-//pinia
+/* PINIA */
 import { useAuthStore } from '@/stores/auth/auth.store';
 
-//login
+/* LOGIN */
 const auth = useAuthStore()
 
-const email = ref('email');
-const password = ref('password')
+const email = ref('');
+const password = ref('');
+
+async function submit() {
+   await auth.login({ email: email.value, password: password.value })
+}
 </script>
