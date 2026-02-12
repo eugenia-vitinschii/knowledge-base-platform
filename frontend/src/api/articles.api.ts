6a20097ac37fcaf5e8deb6e1ff71982650b4f6e8
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-12 17:37:38 +0200

    feat(articles.api): add updateStatus, implement UpdateArticlePayload and UpdateArticleStatusPayload types

--- frontend/src/api/articles.api.ts
+++ frontend/src/api/articles.api.ts
@@ -3,15 +3,20 @@
 import { api } from "./http"
 import type { CreateArticlePayload } from "@/types/create-article.payload"
 import type { Article } from "@/types/article.types"
+import type { UpdateArticlePayload } from "@/types/update-article.payload"
+import type { UpdateArticleStatusPayload } from "@/types/update-article-status.payload"
 
 export const articlesApi = {
    create(payload: CreateArticlePayload) {
       return api.post<Article>('/articles', payload)
    },
-   update(id: string, payload: Partial<CreateArticlePayload>) {
-      return api.put<Article>(`/articke/${id}`, payload)
+   update(id: string, payload: UpdateArticlePayload) {
+      return api.put<Article>(`/articles/${id}`, payload)
+   },
+   updateStatus(id: string, payload: UpdateArticleStatusPayload) {
+      return api.patch<Article>(`/articles/${id}/status`, payload)
    },
    getById(id: string) {
-      return api.get<Article>(`/article/${id}`)
+      return api.get<Article>(`/articles/${id}`)
    }
 }
