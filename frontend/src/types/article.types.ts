a09e59211a03d7d2573cbd47f3f579ca2a828a04
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-24 14:53:02 +0200

    feat(article.types): update article status

--- frontend/src/types/article.types.ts
+++ frontend/src/types/article.types.ts
@@ -1,19 +1,8 @@
 //types/article types
+import type { ArticlePreview } from "./article-preview.type";
 
-import type { ArticleCategory, ArticleDifficulty, ArticleStatus, ArcticleType } from "@/shared/enums/article.enum";
-
-export interface Article {
-   id: string;
-   title: string;
-   slug: string;
-   content: string;
-   difficulty: ArticleDifficulty;
-   tags: string[];
-   category: ArticleCategory;
-   subcategory?: string;
-   type: ArcticleType;
-   status: ArticleStatus;
+export interface Article extends ArticlePreview {
    author: string;
    createdAt: string;
-   updatedAt: string;
 }
+
