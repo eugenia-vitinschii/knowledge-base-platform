914473a039beef6ac112838d17f7bb6b9e67621a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-24 15:49:46 +0200

    feat(HomePage): create basic HTML

--- frontend/src/pages/public/HomePage.vue
+++ frontend/src/pages/public/HomePage.vue
@@ -1,7 +1,18 @@
 <template>
-   <p>home</p>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper">
+            <div class="page__header">
+               <h1 class="heading">HOME</h1>
+            </div>
+            <div class="page__content">
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script setup lang="ts">
-
+/* VUE */
+/* COMPONNETS */
 </script>
