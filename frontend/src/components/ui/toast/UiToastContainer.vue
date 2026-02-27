04dd71430eb3f59981f733630a1c53b2eb7cc2ca
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-02-27 17:19:21 +0200

    feat(UiToastContainer):implement TransitionGroup

---
+++ frontend/src/components/ui/toast/UiToastContainer.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="toast-container">
+      <TransitionGroup name="toast">
+         <ui-toast v-for="toast in store.toasts" :key="toast.id" :toast="toast" />
+      </TransitionGroup>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import UiToast from './UiToast.vue';
+
+/* PINIA*/
+import { useToastStore } from '@/stores/ui/toast.store';
+
+/* variables */
+const store = useToastStore()
+</script>
