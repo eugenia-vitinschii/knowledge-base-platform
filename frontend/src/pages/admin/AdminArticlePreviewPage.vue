31e887875eb18b3c4093165b46cb8cbe939332de
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-03-07 18:33:33 +0200

    refactor(AdminArticlePreviewPage): use articlesCrud, articlesAdmin stores instad of legacy article store

--- frontend/src/pages/admin/AdminArticlePreviewPage.vue
+++ frontend/src/pages/admin/AdminArticlePreviewPage.vue
@@ -4,8 +4,8 @@
          <div class="page__header">
             <h1 class="heading">Article Preview</h1>
          </div>
-         <div class="page__wrapper" v-if="articles.currentArticle">
-            <article-preview :article="articles.currentArticle" :show-admin-controls=true @edit="handleEdit"
+         <div class="page__wrapper" v-if="aticlesAdminStore.currentArticle">
+            <article-preview :article="aticlesAdminStore.currentArticle" :show-admin-controls=true @edit="handleEdit"
                @delete="handleDelete" />
          </div>
       </div>
@@ -16,22 +16,32 @@
 /* COMPONENTS */
 import ArticlePreview from '@/components/article/ArticlePreview.vue';
 
-/* VUE  & PINIA*/
-import { useArticlesStore } from '@/stores/articles/article.store';
+/* PINIA */
+import { useArticlesCrudStore } from '@/stores/articles/article.crud.store';
+import { useArticlesAdminStore } from '@/stores/articles/article.admin.store';
+
+/* VUE  & ROUTER */
 import { useRoute, useRouter } from 'vue-router';
 import { onMounted } from 'vue';
+
 import { useToast } from '@/shared/composables/useToast';
+
+/* PINIA  Variables */
+const articlesCrudStore = useArticlesCrudStore()
+const aticlesAdminStore = useArticlesAdminStore()
+
 /* Variables */
-const articles = useArticlesStore()
 const route = useRoute()
 const router = useRouter();
 const toast = useToast()
-/* fetch article (id)*/
+
+/* FETCH ARTICLES (BY ID)  */
 onMounted(async () => {
    const id = route.params.id as string
 
    if (!id) return
-   await articles.fetchById(id)
+
+   await aticlesAdminStore.fetchById(id)
    toast.info("You are viewing a preview")
 })
 
@@ -45,8 +55,9 @@ const handleDelete = async (id: string) => {
    const confirmed = confirm('Delete this article?')
    if (!confirmed) return
 
-   await articles.remove(id)
-   articles.list = articles.list.filter(a => a.id !== id)
+   await articlesCrudStore.remove(id)
+
+   aticlesAdminStore.list = aticlesAdminStore.list.filter(a => a.id !== id)
    toast.info("Article has been deleted")
    router.push(`/admin/articles`)
 }
