d9bd6bbdfbb9277631eb7d1f71b8431544720d34
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-03-13 15:41:29 +0200

    feat(AppHeader): add user name, role and hide options for editor and viewer roles

--- frontend/src/components/layout/AppHeader.vue
+++ frontend/src/components/layout/AppHeader.vue
@@ -2,15 +2,61 @@
    <header class="header">
       <div class="container">
          <div class="header__wrapper">
-            <router-link to="/" class="body-text">home</router-link>
-            <router-link to="/admin/articles/create" class="body-text">create article</router-link>
-            <router-link to="/admin/articles" class="body-text">all articles</router-link>
-            <router-link to="/login" class="body-text">login</router-link>
-            <router-link to="/article/what-is-vue-js" class="body-text">Public article</router-link>
+            <div class="header__wrapper-public">
+               <router-link to="/">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                     fill="#e3e3e3">
+                     <path
+                        d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+                  </svg>
+               </router-link>
+               <router-link to="/login">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                     fill="#e3e3e3">
+                     <path
+                        d="M367-527q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm296.5-343.5Q560-607 560-640t-23.5-56.5Q513-720 480-720t-56.5 23.5Q400-673 400-640t23.5 56.5Q447-560 480-560t56.5-23.5ZM480-640Zm0 400Z" />
+                  </svg>
+               </router-link>
+            </div>
+            <div class="header__wrapper-admin" v-if="isAdmin || isEditor">
+               <router-link to="/admin/articles/create"><svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                     viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                     <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                  </svg></router-link>
+               <router-link to="/admin/articles"><svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                     viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                     <path
+                        d="M280-280h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm-80 480q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" />
+                  </svg></router-link>
+            </div>
+            <div class="header__wrapper-admin" v-if="isAdmin">
+               <router-link to="/admin/articles">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                     fill="#e3e3e3">
+                     <path
+                        d="M287-527q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm96.5-143.5Q760-287 760-320t-23.5-56.5Q713-400 680-400t-56.5 23.5Q600-353 600-320t23.5 56.5Q647-240 680-240t56.5-23.5Zm-280-320Q480-607 480-640t-23.5-56.5Q433-720 400-720t-56.5 23.5Q320-673 320-640t23.5 56.5Q367-560 400-560t56.5-23.5ZM400-640Zm12 400Z" />
+                  </svg>
+               </router-link>
+            </div>
+            <div class="header__info">
+               <p class="body-text">Hi, {{ auth.user?.name }}! [role: {{ auth.user?.role }}] </p>
+            </div>
          </div>
       </div>
    </header>
 </template>
 
 <script setup lang="ts">
+/* VUE */
+import { computed } from 'vue';
+
+/*  PINIA  */
+import { useAuthStore } from '@/stores/auth/auth.store';
+
+/*PINIA  variables */
+const auth = useAuthStore();
+
+/* check role & fetch data */
+const isAdmin = computed(() => auth.user?.role === 'admin')
+const isEditor = computed(() => auth.user?.role === 'editor')
 </script>
