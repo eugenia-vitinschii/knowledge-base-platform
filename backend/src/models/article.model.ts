f3374d3c121bf5b1b8b5dd8fdd5d9a8400ae69e4
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-03-13 16:58:48 +0200

    feat(article.model) add views and indexex for slug, status, author, category and difficulty

--- backend/src/models/article.model.ts
+++ backend/src/models/article.model.ts
@@ -14,38 +14,38 @@ export interface IArticle extends Document {
    type: ArcticleType
    status: ArticleStatus
    author: Types.ObjectId
-
+   views: number
 }
 
 const ArticleSchema = new Schema<IArticle>({
    title: { type: String, required: true },
    slug: { type: String, required: true, unique: true },
    content: { type: String, required: true },
-   difficulty: { type: String, enum: Object.values(ArticleDifficulty), requred: true },
+   difficulty: { type: String, enum: Object.values(ArticleDifficulty), required: true },
    tags: [{ type: String }],
    category: { type: String, enum: Object.values(ArticleCategory), required: true },
    subcategory: { type: String },
    type: { type: String, enum: Object.values(ArcticleType), required: true },
    status: { type: String, enum: Object.values(ArticleStatus), default: ArticleStatus.DRAFT },
    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+   views: { type: Number, default: 0 }
 },
    { timestamps: true }
 )
+//indexes
+ArticleSchema.index({ slug: 1 })
+ArticleSchema.index({ status: 1 })
+ArticleSchema.index({ author: 1 })
+ArticleSchema.index({ category: 1 })
+ArticleSchema.index({ difficulty: 1 })
+
 
 ArticleSchema.set("toJSON", {
-   transform: (_doc, ret) => {
+   transform: (_doc, ret: any) => {
       ret.id = ret._id.toString();
       delete ret._id;
       delete ret.__v;
    }
 })
-
-
-// ArticleSchema.set("toJSON", {
-//    transform: (_, ret) => {
-//       ret.id = ret._id.toString();
-//       delete ret._id;
-//    }
-// })
 
 export const ArticleModel = mongoose.model<IArticle>("Article", ArticleSchema)
