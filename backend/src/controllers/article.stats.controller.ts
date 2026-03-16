e6f7e0a7bf069e1d161d32adc6f9f4fb7e423352
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-03-16 16:10:42 +0200

    feat(article.stats.controller): add controller for article statistics

---
+++ backend/src/controllers/article.stats.controller.ts
@@ -0,0 +1,20 @@
+//article stats (admin)
+
+import { NextFunction, Request, Response } from "express"
+
+/* SERVICE */
+import { articleStatsService } from "../services/article.stats.service"
+
+class ArticleStatsController {
+   /* GET ALL STATS (status, difficulty, category, type, author) */
+   getOverviewStats = async (req: Request, res: Response, next: NextFunction) => {
+      try {
+         const articlesByStatus = await articleStatsService.getOverviewStats(req.user!)
+
+         res.json(articlesByStatus)
+      } catch (error) {
+         next(error)
+      }
+   }
+}
+export const articleStatsController = new ArticleStatsController()
