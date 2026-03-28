778c1cd90e36e4c6447f7bca8ad71e8acf7347a0
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-03-28 18:36:47 +0200

    feat(HomePage): sync url ans store, fetch filtered articles

--- frontend/src/pages/public/HomePage.vue
+++ frontend/src/pages/public/HomePage.vue
@@ -3,11 +3,12 @@
       <div class="container">
          <div class="page__wrapper">
             <div class="page__header">
-               <h1 class="heading">HOME</h1>
+               <h1 class="heading">HOME </h1>
             </div>
             <div class="page__content">
                <div class="filter-wrapper">
-                  <articles-public-filter v-model:filter="articlePublicStore.filters" />
+                  <articles-public-filter :filter="articlePublicStore.filters" :count="articlePublicStore.list.length"
+                     @update:filter="onFilterChange" />
                </div>
                <div class="article-list" v-if="articlePublicStore.list.length">
                   <article-list-item v-for="article in articlePublicStore.list" :key="article.slug"
@@ -24,6 +25,7 @@
 
 <script setup lang="ts">
 /* VUE */
+import { watch } from 'vue';
 
 /* COMPONNETS */
 import ArticleListItem from '@/components/article/ArticleListItem.vue';
@@ -31,17 +33,49 @@ import ArticlesPublicFilter from '@/components/article/ArticlesPublicFilter.vue'
 
 /*Pinia */
 import { useArticlesPublicStore } from '@/stores/articles/article.public.store';
-import { onMounted, watch } from 'vue';
+import { useArticleFilter } from "@/shared/composables/useArticleFilters"
+
+/* TYPES */
+import type { ArticlePublicFilters } from '@/types/article';
+
+/* ROUTE */
+import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 
 const articlePublicStore = useArticlesPublicStore()
 
-onMounted(() => {
-   articlePublicStore.searchArticles()
-})
+const route = useRoute()
+const router = useRouter()
 
+const { mapQueryToFilters } = useArticleFilter()
+
+
+/*remove empty values from URL */
+function cleanQuery(filters: ArticlePublicFilters) {
+   const query: any = {}
+
+   Object.entries(filters).forEach(([key, value]) => {
+      if (value !== "" && value !== undefined) {
+         query[key] = value
+      }
+   })
+   return query
+}
+/*update URL query when filters change */
+function onFilterChange(newFilters: ArticlePublicFilters) {
+   router.push({
+      path: '/articles',
+      query: cleanQuery(newFilters)
+   })
+}
+/* sync URL => store, fetch filtered articles*/
 watch(
-   () => articlePublicStore.filters,
-   () => { articlePublicStore.searchArticles(articlePublicStore.filters) },
-   { deep: true }
+   () => route.query,
+   async (query) => {
+      const filters = mapQueryToFilters(query)
+      articlePublicStore.filters = filters
+      await articlePublicStore.searchArticles(filters)
+   },
+   { immediate: true }
 )
 </script>
