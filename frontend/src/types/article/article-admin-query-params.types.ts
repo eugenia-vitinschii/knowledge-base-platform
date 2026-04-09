7f7fe81846ea67fef7feae7f4ffefa665e4fd73c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-04-09 17:48:10 +0300

    feat(article-pagination): create ArticleAdminQueryParams type

---
+++ frontend/src/types/article/article-admin-query-params.types.ts
@@ -0,0 +1,9 @@
+//query params
+import type { ArticleAdminFilters } from "./article-admin-filters.types"
+
+
+export type ArticleAdminQueryParams = ArticleAdminFilters & {
+   page?: number
+   limit?: number
+   total?: number
+}
--- frontend/src/types/article/index.ts
+++ frontend/src/types/article/index.ts
@@ -10,6 +10,8 @@ export * from './article-table.types'
 export * from './article.types'
 export * from './article-paginated-response.types'
 export * from './article-query-params.types'
+export * from './article-admin-query-params.types'
+
 /* PAYLOADS */
 export * from './payloads/create-article.payload'
 export * from './payloads/update-article-status.payload'
