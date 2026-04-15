abd2b16901a115f574fa1922d4c2788a0a2e9606
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-04-15 19:04:42 +0300

    feat(admin-users): add position, location, phone, gender,  bio and birth, hide date to User model

---
+++ backend/src/modules/users/models/user.model.ts
@@ -0,0 +1,48 @@
+//user model
+
+import mongoose, { Schema, Document } from "mongoose";
+import { Role } from "../../../common/enums/role.enum.js";
+import { Position, Location, Gender } from "../../../common/enums/user.enums.js";
+
+export interface IUser extends Document {
+   name: string
+   email: string
+   password: string
+   role: Role
+
+   position: Position
+   location: Location
+   phone: string
+
+   gender: Gender
+   bio: string
+
+   birthdate: Date
+   hiredDate: Date
+
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+   name: { type: String, required: true },
+   email: { type: String, required: true, unique: true },
+   password: { type: String, required: true },
+   role: {
+      type: String,
+      enum: Object.values(Role),
+      default: Role.VIEWER,
+   },
+   position: { type: String, enum: Object.values(Position), default: Position.OTHER },
+   location: { type: String, enum: Object.values(Location), default: Location.REMOTE },
+   phone: { type: String },
+
+   gender: { type: String, enum: Object.values(Gender), default: Gender.OTHER },
+   bio: { type: String, maxlength: 500 },
+
+   birthdate: { type: Date },
+   hiredDate: { type: Date },
+
+}, { timestamps: true })
+
+
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema)
