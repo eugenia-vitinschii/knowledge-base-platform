90be066047212c29f35910c7a46e5e40744d371c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-04-15 16:05:14 +0300

    feat(admin-users): create UserTable compoent

---
+++ frontend/src/components/user/UserTable.vue
@@ -0,0 +1,45 @@
+<template>
+   <table class="user-table">
+      <thead>
+         <tr>
+            <th>name</th>
+            <th>role</th>
+            <th>[job title]</th>
+            <th>[departament]</th>
+            <th>actions</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr v-for="i in items">
+            <td>{{ i.name }}</td>
+            <td>{{ i.role }}</td>
+            <td>job title</td>
+            <td>departament</td>
+            <td class="user-table__actions">
+               <ui-button>Edit</ui-button>
+               <ui-button type="button" variant="danger" @click="$emit('delete', i.id)">Delete</ui-button>
+            </td>
+         </tr>
+      </tbody>
+   </table>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import UiButton from '@/components/ui/UiButton.vue';
+
+/* TYPES */
+import type { UserTableRow } from '@/types/user';
+
+/* PROPS */
+const props = defineProps<{
+   items: UserTableRow[];
+}>();
+
+/* EMITS */
+const emit = defineEmits<{
+   (e: 'delete', id: string): void;
+}>();
+
+
+</script>
--- frontend/src/types/user/index.ts
+++ frontend/src/types/user/index.ts
@@ -1,6 +1,7 @@
 
 /* PAYLOADS */
-export * from  "./payloads/create-user.payload"
+export * from "./payloads/create-user.payload"
 export * from "./payloads/update-user-role.payload"
 /* TYPES */
-export * from  "./types/user.type"
+export * from "./types/user.type"
+export * from "./types/user-table.types"
