0b16427b435b185e259d2d60bc21c4534eec746e
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-04-18 18:49:15 +0300

    feat(admin-users): create user enums: Position, Location, Gender

---
+++ frontend/src/shared/enums/user.enum.ts
@@ -0,0 +1,25 @@
+//user enums
+
+export enum Position {
+   FRONTEND = 'Frontend Developer',
+   BACKEND = "Backend Developer",
+   FULLSTACK = "Fullstack Developer",
+   DEVOPS = "DevOps Engineer",
+   QA = "QA Engineer",
+   UI_UX = "UI/UX Designer",
+   DATA_SCIENTIST = "Data Scientist",
+   PRODUCT_MANAGER = "Product Manager",
+   OTHER = "Other"
+}
+
+export enum Location {
+   REMOTE = 'Remote',
+   ONSITE = 'Onsite',
+   HYBRID = 'Hybrid'
+}
+
+export enum Gender {
+   MALE = "Male",
+   FEMALE = "Female",
+   OTHER = "Other"
+}
