e22ce045608e4aff1637f73f71bea13686e7052c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-04-29 15:34:09 +0300

    feat(profile): create reusable validation fields

---
+++ frontend/src/shared/validation/users.fields.ts
@@ -0,0 +1,23 @@
+// users + profile fields (admin/public)
+
+import { z } from 'zod'
+
+export const nameField = z
+   .string({ error: "Name is required" })
+   .trim().min(5, "Minimum 5 characters")
+   .max(50, 'Maximum 50 characters')
+
+export const emailField = z
+   .string({ error: "Email is required" })
+   .trim()
+   .email("Invalid email adress")
+
+export const phoneField = z
+   .string()
+   .trim()
+   .max(30, 'Too long ')
+   .optional()
+
+export const bioField = z
+   .string().max(500, 'Maximum 500 characters').optional()
+
