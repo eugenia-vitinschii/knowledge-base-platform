08cadbcf9bf9765cf9f16256c3415af9875c532d
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-12 18:14:48 +0300

    feat(ui): create icon button component

---
+++ frontend/src/components/ui/UiIconButton.vue
@@ -0,0 +1,32 @@
+<template>
+   <button class="icon-btn" :class="[`icon-btn--${variant}`, { 'is-loading': loading }]" :disabled="disabled"
+      :type="type">
+      <span class="icon-btn__content" v-if="!loading">
+         <slot></slot>
+      </span>
+   </button>
+</template>
+
+<script setup lang="ts">
+/* BUTTON TYPES */
+type ButtonVariant = "primary" | "danger"
+// type ButtonSize = "sm" | "md" | "lg"
+type ButtonType = "button" | "submit"
+
+/* PROPS INTERFACE*/
+interface Props {
+   variant?: ButtonVariant
+   // size?: ButtonSize
+   type?: ButtonType
+   loading?: boolean
+   disabled?: boolean
+}
+/* DEFAULT BUTTON Props */
+withDefaults(defineProps<Props>(), {
+   variant: "primary",
+   type: "button",
+   // size: "lg",
+   loading: false,
+   disabled: false
+})
+</script>
