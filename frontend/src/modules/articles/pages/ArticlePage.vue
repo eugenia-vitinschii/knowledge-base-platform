21bd09f98a3ef94dac5dab04c2400212c132e1f7
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-13 15:37:09 +0300

    feat(articles): intergate comments system into article page. Closes  #29

--- frontend/src/modules/articles/pages/ArticlePage.vue
+++ frontend/src/modules/articles/pages/ArticlePage.vue
@@ -4,8 +4,10 @@
          <div class="page__header">
             <h1 class="heading">Article Page</h1>
          </div>
-         <div class="page__wrapper" v-if="articles.currentPreview">
-            <article-preview :article="articles.currentPreview" :show-admin-controls=false />
+         <div class="page__wrapper" v-if="articlesStore.currentPreview">
+            <article-preview :article="articlesStore.currentPreview" :show-admin-controls=false />
+            <comment-list :comments="commentsStore.comments" />
+            <comment-form @submit="onSubmit" />
          </div>
          <div v-else>
             <p class="body-text">Article not found</p>
@@ -15,29 +17,56 @@
 </template>
 
 <script setup lang="ts">
+/* VUE & ROUTER*/
+import { useRoute } from 'vue-router';
+import { watch, computed } from 'vue'
+
+/* STORES */
+import { useArticlesPublicStore } from '../store/article.public.store';
+import { useComentsStore } from '@/modules/comments/store/comment.store';
+import { useToast } from '@/shared/composables/useToast';
+
 /* COMPONENTS */
 import ArticlePreview from '../components/ArticlePreview.vue';
+import CommentForm from '@/modules/comments/components/CommentForm.vue';
+import CommentList from '@/modules/comments/components/CommentList.vue';
 
-/* VUE  & PINIA & ROUTER*/
-import { useArticlesPublicStore } from '../store/article.public.store';
-import { useRoute } from 'vue-router';
-import { watch } from 'vue'
+import type { CreateCommentPayload } from '@/modules/comments/types';
 
 /* Variables */
-const articles = useArticlesPublicStore()
+const toast = useToast()
 const route = useRoute()
 
+const articlesStore = useArticlesPublicStore()
+const commentsStore = useComentsStore()
+
+const articleId = computed(() => articlesStore.currentPreview?.id || '')
 
 async function loadArticle(slug: string) {
 
    const key = `viewed-${slug}`
 
    if (!sessionStorage.getItem(key)) {
-      await articles.incrementViews(slug)
+      await articlesStore.incrementViews(slug)
       sessionStorage.setItem(key, 'true')
    }
-   await articles.fetchBySlug(slug)
+   await articlesStore.fetchBySlug(slug)
 
+   if (articlesStore.currentPreview?.id) {
+      await commentsStore.fetchByArticle(articlesStore.currentPreview.id)
+   }
+
+}
+
+
+/* submit */
+async function onSubmit(payload: CreateCommentPayload) {
+
+   const comment = await commentsStore.create(articleId.value, payload)
+
+   if (!comment) return toast.error("Comment wasn't created")
+
+   toast.success("Comment was created")
 }
 
 watch(
@@ -49,4 +78,5 @@ watch(
    },
    { immediate: true }
 )
+
 </script>
