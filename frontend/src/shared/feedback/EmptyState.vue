71b4e29e3b5ced2590f01a84b4d80ece0eb52d42
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-15 14:59:54 +0300

    feat(empty-state): create reusable component

---
+++ frontend/src/shared/feedback/EmptyState.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="empty-state" :class="`empty-state--${variant}`">
+      <div class="empty-state__text">
+         <h3 class="subheading">{{ title }}</h3>
+         <p class="body-text">{{ description }}</p>
+      </div>
+      <div class="empty-state__actions">
+         <slot name="action"></slot>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* VARIANTS */
+type Variant = 'default' | 'accent' | 'search'
+
+withDefaults(
+   defineProps<{
+      title?: string
+      description?: string
+      variant?: Variant
+   }>(),
+   {
+      variant: 'default'
+   }
+)
+
+</script>
