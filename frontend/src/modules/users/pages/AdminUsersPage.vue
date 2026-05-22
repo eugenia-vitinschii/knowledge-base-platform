195431cd1ac65ea997a7a66820075a10fd82a253
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-22 19:37:19 +0300

    refactor(admin-users): implement standardized error handling and loading flow -restructurized store actions using try/catch/finally - add global isLoading and error states to template component - converted direct store state access in templares to local computed values - enabled dynamic DEBUG_API_FAIL feature for testing

--- frontend/src/modules/users/pages/AdminUsersPage.vue
+++ frontend/src/modules/users/pages/AdminUsersPage.vue
@@ -11,12 +11,17 @@
                   <div class="user-table__wrapper" v-if="isLoading" key="loading">
                      <table-skeleton :rows="9" :buttons="2" :columns="5" />
                   </div>
+                  <div class="page__info" v-else-if="error" key="error">
+                     <error-state title="Oops! Something went wrong..."
+                        description="Failed to load users. It might be a temporary connection issue. Please check your internet or try refreshing the page."
+                        buttonText="Try Again" @retry="handleRetry" />
+                  </div>
                   <div class="user-table__wrapper" v-else-if="hasUsers" key="users">
-                     <user-table :items="userStore.list" @delete="handleDeleteUser" @edit="handleEdit" />
+                     <user-table :items="users" @delete="handleDeleteUser" @edit="handleEdit" />
                   </div>
                   <div class="empty-state__wrapper" v-else-if="!isLoading && hasUsers" key="empty">
-                     <empty-state :variant="'search'" :title="'No results found'"
-                        :description="'Try adjusting your filters or search terms to find what youre looking for'" />
+                     <empty-state variant="search" title="No results found"
+                        description="Try adjusting your filters or search terms to find what youre looking for" />
                   </div>
                </Transition>
             </div>
@@ -27,29 +32,47 @@
 </template>
 
 <script setup lang="ts">
-/* COMPONENTS */
-import UserTable from '@/modules/users/components/UserTable.vue';
-import TableSkeleton from '@/shared/ui/TableSkeleton.vue';
-import EmptyState from '@/shared/feedback/EmptyState.vue';
-
-/* PINIA */
-import { useAdminUsersStore } from '@/modules/users/store/admin.users.store';
-import { useToast } from '@/shared/composables/useToast';
-
 /* VUE & ROUTER */
 import { onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
+/* STORE & COMPOSABLES */
+import { useAdminUsersStore } from '@/modules/users/store/admin.users.store';
+import { useToast } from '@/shared/composables/useToast';
 
+/* COMPONENTS */
+import UserTable from '@/modules/users/components/UserTable.vue';
+import TableSkeleton from '@/shared/ui/TableSkeleton.vue';
+import EmptyState from '@/shared/feedback/EmptyState.vue';
+import ErrorState from '@/shared/feedback/ErrorState.vue'
+
+/* === ROUTER & STORES === */
 const userStore = useAdminUsersStore();
 const toast = useToast()
 const router = useRouter();
 
+/* === STATE COMPUTED === */
 /* UI render flow*/
 const isLoading = computed(() => userStore.isLoading)
 const hasUsers = computed(() => userStore.list.length > 0)
+const error = computed(() => userStore.error)
+const users = computed(() => userStore.list)
+/* === WATCHERS === */
+onMounted(() => {
+   userStore.fetchUsers()
+})
 
-/* ACTIONS */
+/*=== EVENT HANDLERS === */
+/* retry action  */
+function handleRetry() {
+   userStore.fetchUsers()
+}
+/* edit user */
+const handleEdit = (id: string) => {
+   if (!id) return
+   router.push(`/admin/users/edit/${id}`)
+}
+/* delete user */
 const handleDeleteUser = async (id: string) => {
    const confirmed = confirm('Delete this user')
    if (!confirmed) return
@@ -62,13 +85,4 @@ const handleDeleteUser = async (id: string) => {
 
 };
 
-const handleEdit = (id: string) => {
-   if (!id) return
-   router.push(`/admin/users/edit/${id}`)
-}
-
-
-onMounted(() => {
-   userStore.fetchUsers()
-})
 </script>
--- frontend/src/modules/users/store/admin.users.store.ts
+++ frontend/src/modules/users/store/admin.users.store.ts
@@ -5,28 +5,37 @@ import { ref } from "vue";
 
 import { usersApi } from "@/modules/users/api/users.api";
 
-/*TYPES */
+/* TYPES */
 import type { AdminUpdateUserPayload, AdminCreateUserPayload, UpdateUserRolePayload, User } from "@/modules/users/types";
 
-/* COMPOSABLE */
+/* COMPOSABLE & LIBS */
 import { useApiRequest } from "@/shared/composables/useApiRequest";
 import { delay } from "@/shared/lib/delay"
 
 export const useAdminUsersStore = defineStore("adminUsers", () => {
-
+   const { request } = useApiRequest()
+   /* === STATE === */
    const selectedUser = ref<User | null>(null);
    const list = ref<User[]>([])
 
-   const { request } = useApiRequest()
+   /* === UI FLOW STATE === */
    const isLoading = ref(false)
+   const error = ref<string | null>(null)
 
-   /* === FETCH USERS === */
+   /* === PORTFOLIO FEATURE === */
+   const DEBUG_API_FAIL = ref(false)
+
+   /* === ACTIONS === */
+   /*  Fetch all users  */
    async function fetchUsers() {
       isLoading.value = true
-
+      error.value = null
       try {
          if (import.meta.env.DEV) {
             await delay(800)
+         }
+         if (import.meta.env.DEV && DEBUG_API_FAIL.value) {
+            throw new Error('Mock error')
          }
          const data = await request(() =>
             usersApi.admin.getAll().then(r => r.data),
@@ -38,83 +47,126 @@ export const useAdminUsersStore = defineStore("adminUsers", () => {
          }
 
          return data
+      } catch (err) {
+         error.value = "Failed to fetch users"
       } finally {
          isLoading.value = false
       }
 
    }
-   /* === Fetch user by id === */
+   /* Fetch user by id*/
    async function fetchUserById(id: string) {
-      const data = await request(() =>
-         usersApi.admin.getById(id).then(r => r.data),
-         "Failed to load user"
-      )
+      isLoading.value = true
+      error.value = null
 
-      if (data) {
-         selectedUser.value = data
+      try {
+         const data = await request(() =>
+            usersApi.admin.getById(id).then(r => r.data),
+            "Failed to load user"
+         )
+
+         if (data) {
+            selectedUser.value = data
+         }
+
+         return data
+      } catch (err) {
+         error.value = "Failed to fetch user"
+      } finally {
+         isLoading.value = false
       }
 
-      return data
    }
-   /* === CREATE USER === */
+   /* Create user */
    async function create(payload: AdminCreateUserPayload) {
-
-      const data = await request(() =>
-         usersApi.admin.create(payload).then(r => r.data),
-         "Failed to create new user"
-      )
-
-      if (data) {
-         await fetchUsers()
+      isLoading.value = true
+      try {
+         const data = await request(() =>
+            usersApi.admin.create(payload).then(r => r.data),
+            "Failed to create new user"
+         )
+         if (data) {
+            await fetchUsers()
+         }
+         return data
+      } catch (err) {
+         return null
+      } finally {
+         isLoading.value = false
       }
-      return data
+
    }
-   /* === UPDATE ROLE === */
+   /* === Update role  === */
    async function updateRole(id: string, payload: UpdateUserRolePayload) {
+      isLoading.value = true
+      try {
+         const data = await request(() =>
+            usersApi.admin.updateRole(id, payload).then(r => r.data),
+            "Failed to update user role"
+         )
 
-      const data = await request(() =>
-         usersApi.admin.updateRole(id, payload).then(r => r.data),
-         "Failed to update user role"
-      )
-
-      if (data) {
-         selectedUser.value = data
+         if (data) {
+            selectedUser.value = data
+         }
+         return data
+      } catch (err) {
+         return null
+      } finally {
+         isLoading.value = false
       }
-      return data
    }
-   /* === UPDATE USER  === */
+   /* === Update user === */
    async function update(id: string, payload: AdminUpdateUserPayload) {
-      const data = await request(() =>
-         usersApi.admin.update(id, payload).then(r => r.data),
-         "Failed to update user"
-      )
+      isLoading.value = true
+      try {
+         const data = await request(() =>
+            usersApi.admin.update(id, payload).then(r => r.data),
+            "Failed to update user"
+         )
 
-      if (data) {
-         selectedUser.value = data
+         if (data) {
+            selectedUser.value = data
+         }
+         return data
+      } catch (err) {
+         return null
+      } finally {
+         isLoading.value = false
       }
-      return data
+
    }
-   /* === DETELE USER === */
+   /* === Delete user=== */
    async function remove(id: string) {
-      const data = await request(() =>
-         usersApi.admin.delete(id),
-         "Failed to delete user"
-      )
+      isLoading.value = true
+      try {
+         const data = await request(() =>
+            usersApi.admin.delete(id),
+            "Failed to delete user"
+         )
 
-      if (!data) return false
-      list.value = list.value.filter((a) => a.id !== id)
+         if (!data) return false
+         list.value = list.value.filter((a) => a.id !== id)
 
-      return true
+         return true
+      } catch (err) {
+         return false
+      } finally {
+         isLoading.value = false
+      }
    }
    return {
+      //state
       selectedUser,
       list,
+      isLoading,
+      error,
+      DEBUG_API_FAIL,
+      //Actions 
       create,
       updateRole,
       remove,
       fetchUsers,
       update,
       fetchUserById,
-      isLoading
    }
 })
