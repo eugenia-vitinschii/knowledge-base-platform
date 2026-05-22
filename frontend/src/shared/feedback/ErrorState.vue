3f9d5ada07b4aaedd83ef0d93c16f8d43ade3143
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-22 19:19:29 +0300

    feat(error-state): create reusabke component. Closes #32

---
+++ frontend/src/shared/feedback/ErrorState.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="error-state">
+      <div class="error-state__text">
+         <h3 class="subheading">{{ title }}</h3>
+         <p class="body-text">{{ description }}</p>
+      </div>
+      <div class="error-state__action" v-if="buttonText">
+         <ui-button @click="$emit('retry')">
+            {{ buttonText }}
+         </ui-button>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import UiButton from '@/components/ui/UiButton.vue'
+
+/* PROPS */
+const props = withDefaults(
+   defineProps<{
+      title?: string
+      description?: string
+      buttonText?: string
+   }>(),
+   {
+      title: "Oops! Something went wrong",
+      description: 'Failed to fetch data. Please try again.',
+   }
+)
+/* EMITS */
+const emit = defineEmits<{
+   (e: 'retry'): void
+}>()
+
+</script>
