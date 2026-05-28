374b7bac4614adf5acc38dde1a653993371753f9
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-05-28 17:02:40 +0300

    feat(admin-users): implement user filter and pagination. Closes #34

--- frontend/src/modules/users/pages/AdminUsersPage.vue
+++ frontend/src/modules/users/pages/AdminUsersPage.vue
@@ -6,7 +6,9 @@
                <h1 class="heading">Admin Users Page</h1>
             </div>
             <div class="page__content">
-               <div class="filter-wrapper"></div>
+               <div class="filter-wrapper">
+                  <admin-user-filter :filter="filters" :count="totalItems" @update:filter="onFilterChange" />
+               </div>
                <Transition name="fade" mode="out-in">
                   <div class="user-table__wrapper" v-if="isLoading" key="loading">
                      <table-skeleton :rows="9" :buttons="2" :columns="5" />
@@ -19,13 +21,15 @@
                   <div class="user-table__wrapper" v-else-if="hasUsers" key="users">
                      <user-table :items="users" @delete="handleDeleteUser" @edit="handleEdit" />
                   </div>
-                  <div class="empty-state__wrapper" v-else-if="!isLoading && hasUsers" key="empty">
+                  <div class="empty-state__wrapper" v-else-if="!isLoading && !hasUsers && !error" key="empty">
                      <empty-state variant="search" title="No results found"
                         description="Try adjusting your filters or search terms to find what youre looking for" />
                   </div>
                </Transition>
             </div>
-            <div class="page__footer"></div>
+            <div class="page__footer" v-if="hasUsers && !isLoading">
+               <base-pagination :page="currentPage" :total-pages="totalPages" @change="onPageChange" />
+            </div>
          </div>
       </div>
    </div>
@@ -33,23 +37,33 @@
 
 <script setup lang="ts">
 /* VUE & ROUTER */
-import { onMounted, computed } from 'vue';
+import { computed, watch } from 'vue';
 import { useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
 
 /* STORE & COMPOSABLES */
 import { useAdminUsersStore } from '@/modules/users/store/admin.users.store';
 import { useToast } from '@/shared/composables/useToast';
+import { useUserAdminFilter } from "@/modules/users/composables/UseUsersAdminFilters"
+
+/* TYPES */
+import type { UserQueryParams, AdminUserFilters } from '../types/index.ts';
 
 /* COMPONENTS */
+import AdminUserFilter from '../components/AdminUserFilter.vue';
 import UserTable from '@/modules/users/components/UserTable.vue';
 import TableSkeleton from '@/shared/ui/TableSkeleton.vue';
 import EmptyState from '@/shared/feedback/EmptyState.vue';
 import ErrorState from '@/shared/feedback/ErrorState.vue'
+import BasePagination from '@/components/ui/BasePagination.vue';
 
 /* === ROUTER & STORES === */
 const userStore = useAdminUsersStore();
 const toast = useToast()
 const router = useRouter();
+const route = useRoute()
+const { mapQueryToParams } = useUserAdminFilter()
+
 
 /* === STATE COMPUTED === */
 /* UI render flow*/
@@ -57,16 +71,82 @@ const isLoading = computed(() => userStore.isLoading)
 const hasUsers = computed(() => userStore.list.length > 0)
 const error = computed(() => userStore.error)
 const users = computed(() => userStore.list)
-/* === WATCHERS === */
-onMounted(() => {
-   userStore.fetchUsers()
-})
 
-/*=== EVENT HANDLERS === */
-/* retry action  */
-function handleRetry() {
-   userStore.fetchUsers()
+/* filter */
+const filters = computed(() => userStore.filters)
+
+/* pagination  metadata */
+const currentPage = computed(() => userStore.meta?.page ?? 1)
+const totalPages = computed(() => userStore.meta?.pages ?? 1)
+const totalItems = computed(() => userStore.meta?.total ?? 1)
+
+/* === HELPERS === */
+function extractFilters(params: UserQueryParams): AdminUserFilters {
+   const { page, limit, ...filters } = params
+   return filters
 }
+
+/* remove empty values from URL */
+function cleanQuery(params: UserQueryParams) {
+   const query: any = {}
+
+   Object.entries(params).forEach(([key, value]) => {
+      if (value !== "" && value !== undefined) {
+         query[key] = value
+      }
+   })
+   return query
+}
+/* === DATA LOADERS == */
+async function loadUsers() {
+   const params = mapQueryToParams(route.query)
+
+   userStore.filters = extractFilters(params)
+   await userStore.searchUsers(params)
+
+}
+
+/* === WATCHERS === */
+watch(
+   () => route.query,
+   async () => {
+      await loadUsers()
+   },
+   { immediate: true }
+)
+/* === URL SYNC HANDLERS === */
+/*update URL query when filters change */
+function onFilterChange(newFilters: AdminUserFilters) {
+   const params = mapQueryToParams(route.query)
+
+   router.push({
+      path: '/admin/users',
+      query: cleanQuery({
+         ...params,
+         ...newFilters,
+         page: 1,
+         limit: 10
+      })
+   })
+}
+/* pagination  click*/
+function onPageChange(page: number) {
+   const params = mapQueryToParams(route.query)
+
+   router.push({
+      path: '/admin/users',
+      query: cleanQuery({
+         ...params,
+         page
+      })
+   })
+}
+/*=== CRUD EVENT HANDLERS === */
+/* retry action  */
+async function handleRetry() {
+   await loadUsers()
+}
+
 /* edit user */
 const handleEdit = (id: string) => {
    if (!id) return
@@ -81,6 +161,7 @@ const handleDeleteUser = async (id: string) => {
 
    if (success) {
       toast.info("User has been deleted")
+      await loadUsers()
    }
 
 };
