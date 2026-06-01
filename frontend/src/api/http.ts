2dea683bfcf0bdf8801e70d7abbc8fab63c104ba
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-06-01 16:12:29 +0300

    feat(error-pages): implement response interceptor

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,12 +1,14 @@
 //axios interceptor
 
 import axios from "axios";
+import router from "@/router/index"
 
 export const api = axios.create({
    baseURL: import.meta.env.VITE_API_URL,
 });
 
 
+/* request interceptor */
 api.interceptors.request.use((config) => {
    const token = localStorage.getItem("token");
 
@@ -15,4 +17,23 @@ api.interceptors.request.use((config) => {
    }
 
    return config;
+})
+
+/* response interceptor */
+api.interceptors.response.use((response) => response, (error) => {
+   const status = error?.response?.status
+   const currentPatch = router.currentRoute.value.path
+
+   if (status === 401 && currentPatch !== '/401') {
+      localStorage.removeItem("token")
+      router.push('/401')
+   }
+   if (status === 403 && currentPatch !== '/403') {
+      router.push('/403')
+   }
+   if (status >= 500 && currentPatch !== '/500') {
+      router.push('/500')
+   }
+   return Promise.reject(error)
+
 })
