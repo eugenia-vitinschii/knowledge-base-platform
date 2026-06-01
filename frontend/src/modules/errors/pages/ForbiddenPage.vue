6864ea38064a6629ebef043a226411fd8a9b7231
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-06-01 16:06:26 +0300

    feat(errors-pages): create error pages using ErrorPageState component

---
+++ frontend/src/modules/errors/pages/ForbiddenPage.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper  error-page-wrapper">
+            <error-page-state code="403" title="Access Denied" button-text="go to home" @action="goHome"
+               description="You don't have permission to view this page. If you think this is a mistake, please contact your system administrator" />
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* ROUTER */
+import { useRouter } from 'vue-router'
+
+/* COMPONENTS */
+import ErrorPageState from '../components/ErrorPageState.vue';
+
+const router = useRouter()
+
+function goHome() {
+   router.push('/')
+}
+</script>
---
+++ frontend/src/modules/errors/pages/ServerErrorPage.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="page">
+      <div class="container">
+         <div class="page__wrapper  error-page-wrapper">
+            <error-page-state code="500" title="Internal Server Error" button-text="try again" @action="handleReload"
+               description="Something went wrong on our end. Our technical team has been notified, and we are working to fix the issue. Please try again later" />
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import ErrorPageState from '../components/ErrorPageState.vue';
+
+function handleReload() {
+   window.location.reload()
+}
+</script>
