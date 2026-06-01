c923f92275ff8a36432c2c4aa95f409663e1d84a
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-06-01 16:19:19 +0300

    feat(error-pages): implement condition rendering based on route meta

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,31 @@
 <template>
-   <div class="wrapper">
-      <app-header />
-      <div class="content">
-         <router-view />
-         <ui-toast-container />
+   <Transition name="fade" mode="out-in">
+      <div class="wrapper" v-if="route.meta.layout === 'error'">
+         <div class="content">
+            <router-view />
+         </div>
       </div>
-      <app-footer />
-   </div>
-
+      <div class="wrapper" v-else>
+         <app-header />
+         <div class="content">
+            <router-view />
+            <ui-toast-container />
+         </div>
+         <app-footer />
+      </div>
+   </Transition>
 </template>
 
 <script setup lang="ts">
+/* ROUTER */
+import { useRoute } from 'vue-router';
+
+
 /* COMPONENTS */
 import AppHeader from './components/layout/AppHeader.vue';
 import AppFooter from './components/layout/AppFooter.vue';
 import UiToastContainer from './components/ui/toast/UiToastContainer.vue';
+
+const route = useRoute()
 
 </script>
