48757b1ab34e572a16f82af7cdf05d7a54a48078
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-06-01 16:02:47 +0300

    feat(error-pages): create reusable component

---
+++ frontend/src/modules/errors/components/ErrorPageState.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="error-page">
+      <div class="error-page__content">
+         <h1 class="heading">{{ code }}</h1>
+         <h2 class="subheading">{{ title }}</h2>
+         <p class="body-text">{{ description }}</p>
+      </div>
+      <div class="error-page__actions" v-if="buttonText">
+         <ui-button @click="onAction">
+            {{ buttonText }}
+         </ui-button>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS */
+import UiButton from '@/components/ui/UiButton.vue';
+
+/* === PROPS & EMITS === */
+const props = defineProps<{
+   code: string | number,
+   title: string,
+   description: string
+   buttonText?: string
+}>()
+
+const emit = defineEmits<{
+   (e: 'action'): void
+}>()
+
+function onAction() {
+   emit('action')
+}
+</script>
